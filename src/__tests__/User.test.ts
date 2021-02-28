7642784e53b33408a9bd36f2d28f785422da1c04
Jerry Macedo Castro <leom551@hotmail.com>
2021-02-27 22:46:48 -0300

    Create test for user create

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,19 @@
+import { app } from '../app';
+import createConnection from '../database';
+import request from 'supertest';
+
+describe('Users', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it('Should be able to create a new user', async () => {
+    const response = await request(app).post('/user').send({
+      email: 'user@example.com',
+      name: 'user name ',
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
