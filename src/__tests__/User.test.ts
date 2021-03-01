d75cba2d41468fe6c3cf8491102e73f81fbc3992
Jerry Macedo Castro <leom551@hotmail.com>
2021-03-01 19:59:30 -0300

    Add yup to user controller

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,11 +1,17 @@
 import { app } from '../app';
 import createConnection from '../database';
 import request from 'supertest';
+import { getConnection } from 'typeorm';
 
 describe('Users', () => {
   beforeAll(async () => {
     const connection = await createConnection();
     await connection.runMigrations();
+  });
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
   });
 
   it('Should be able to create a new user', async () => {
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,30 +1,38 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
 
     const usersRepository = getCustomRepository(UsersRepository);
 
-    const user = usersRepository.create({
-      name,
-      email,
-    });
-
-    const userAlreadyExists = await usersRepository.findOne({
-      email,
-    });
-    if (userAlreadyExists)
-      return response.status(400).json({ message: 'User already exists' });
-
     try {
+      await schema.validate(request.body, { abortEarly: false });
+
+      const user = usersRepository.create({
+        name,
+        email,
+      });
+
+      const userAlreadyExists = await usersRepository.findOne({
+        email,
+      });
+      if (userAlreadyExists) {
+        return response.status(400).json({ Error: 'User already exists' });
+      }
+
       await usersRepository.save(user);
 
       return response.status(201).json(user);
     } catch (error) {
-      return response.status(400).json({ message: error.message });
+      return response.status(400).json({ Error: error });
     }
   }
 }
