4e2d02d8732c246c8d6ac84112f6ef2975046929
Jerry Macedo Castro <leom551@hotmail.com>
2021-02-28 23:15:27 -0300

    Create endpoint for nps calculus

---
+++ src/controllers/NpsController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { getCustomRepository, Not, IsNull } from 'typeorm';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+class NpsController {
+  async execute(request: Request, response: Response) {
+    const { survey_id } = request.params;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveysUsers = await surveysUsersRepository.find({
+      survey_id,
+      value: Not(IsNull()),
+    });
+
+    const defractores = surveysUsers.filter(
+      (surveyUser) => surveyUser.value >= 0 && surveyUser.value <= 6
+    ).length;
+
+    const promoters = surveysUsers.filter(
+      (surveyUser) => surveyUser.value >= 9 && surveyUser.value <= 10
+    ).length;
+
+    const passives = surveysUsers.filter(
+      (surveyUser) => surveyUser.value >= 7 && surveyUser.value <= 8
+    ).length;
+
+    const totalAnswers = Object.keys(surveysUsers).length;
+
+    const calculus = ((promoters - defractores) / totalAnswers) * 100;
+
+    return response.status(200).json({
+      defractores,
+      promoters,
+      passives,
+      totalAnswers,
+      nps: calculus,
+    });
+  }
+}
+
+export { NpsController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AnswerController } from './controllers/AnswersController';
+import { NpsController } from './controllers/NpsController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
@@ -8,6 +9,7 @@ const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 const router = Router();
 
@@ -19,5 +21,7 @@ router.get('/survey', surveyController.findAll);
 router.post('/sendmail', sendMailController.execute);
 
 router.get('/answers/:value', answerController.execute);
+
+router.get('/nps/:survey_id', npsController.execute);
 
 export { router };
